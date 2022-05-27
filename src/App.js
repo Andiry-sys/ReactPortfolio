55bf5763e9bd4d4a2cbfed8fc521ab044826f39b
Andriy-sys <marchycandriy@gmail.com>
2022-05-27 17:41:22 +0300

    Modify

--- src/App.js
+++ src/App.js
@@ -4,20 +4,50 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>About me</p>
+        <ul>
+          <li>Name: Andriy</li>
+          <li>Surname: Marchyck</li>
+          <li>Age: 17</li>
+          <li>Email: marchycandriY@gmail.com</li>
+          <li>Phone: +380937009900</li>
+        </ul>
+        <p>Skills</p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+          <li>Express</li>
+          <li>MongoDB</li>
+          <li>.NET</li>
+          <li>Angular</li>
+          <li>SQL</li>
+        </ul>
+        <p>Languages</p>
+        <ul>
+          <li>Ukrainian</li>
+          <li>English</li>
+          <li>Russian</li>
+        </ul>
+        <p>Education</p>
+        <ul>
+          <li>IT Step Academy</li>
+        </ul>
+      </header>
+      <footer className="App-Footer">
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Andiry-sys"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
